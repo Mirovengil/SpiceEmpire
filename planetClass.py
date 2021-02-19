1d5bb7e9e0beebf2075c0dd2c13f8772ccb731da
Шумилин Иван <mirovengil@gmail.com>
2021-02-19 17:10:33 +0300

    Базовый класс планеты. Для тестов. Ваня, тебе потом надо будет это счастье расширять.

---
+++ planetClass.py
@@ -0,0 +1,8 @@
+XCR = 0; YCR = 1
+
+class Planet:
+    def __init__(self, name, typ, coordinates):
+        self.name = name
+        self.typ = typ
+        self.coordinates.x = coordinates[XCR]
+        self.coordinates.y = coordinates[YCR]
