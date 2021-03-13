9f20fdf9edaf8391a0dd50891da037435e2323bf
Mirovengil <60517270+Mirovengil@users.noreply.github.com>
2021-03-13 17:00:35 +0300

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,21 +1,28 @@
 from classStar import Star
-from planetClass import Planet
+from classPlanet import Planet
 
 class gameMap:
-    exist = False
-    
     def __init__(self):
-        if gameMap.exist:
-            raise ValueError('Игровая карта может существовать только в едином экземпляре!!11 АУФ!11')
         self.stars = None
         self.shiphs = None
-        gameMap.exist = True
+        self.sizex = None
+        self.sizey = None
+    
+    def setSize(self, sizex, sizey):
+        self.sizex = sizex
+        self.sizey = sizey
+        if self.stars != None:
+            for i in range(len(self.stars)):
+                self.stars[i].setSize(sizex, sizey)
     
     def setStars(self, stars):
         self.stars = stars
-        
+        if self.sizex != None:
+            for i in range(len(self.stars)):
+                self.stars[i].setSize(sizex, sizey)
+                
     def setPlanets(self, planets):
-        if stars == None:
+        if self.stars == None:
             raise ValueError("Сперва задайте звёзды, к которым будут прикрепляться планеты!1")
         for i in range(len(planets)):
             self.stars[i].setPlanets(planets[i])
@@ -26,21 +33,23 @@ class gameMap:
     def __str__(self):
         string = ""
         for i in self.stars:
-            string = string + "-------------------------------------------------------" + "\n"
-            string = string + 'Название звезды: ' + str(i.name) + "\n"
-            string = string + 'Кол-во планет: ' + str(i.planetsNumber()) + "\n"
-            string = string + 'Соседи: ' + "\n"
-            for j in i.neighbours:
-                string = string + '    >' + Star.getNeighbour(j).getName() + ' (' + str(Star.getWayLen(j)) +  'ПА' +  ')' + "\n"
-            string = string + 'Планеты: ' + "\n"
-            for j in i.planets:
-                string = string + '      > Название планеты: ' + j.getName() + "\n"
-                string = string + '           Стратегический тип планеты: ' + j.getType() + "\n"
-                string = string + '           Описание планеты: ' + j.getDescription() + "\n"
-                string = string + '           Координаты в системе: ' + j.getCoordinates() + "\n"
-                string = string + '           Изображение: ' + j.getImage() + "\n"
-                string = string + '           Скорость добычи стали у одного завода: ' + int(j.getSteel() * 100) + '%' + "\n"
-                string = string + '           Скорость добычи еды у одной фермы: ' + int(j.getFood() * 100) + '%' + "\n"
-                string = string + '           Скорость получения денег у одного порта: ' + int(j.getMoney() * 100) + '%' + "\n"
-            string = string + '-------------------------------------------------------' + "\n"
-        
+           string = string + i.str(self.stars)
+        return string
+
+    def cache(self):
+        string = ""
+        string = string + str(self.sizex) + "\n"
+        string = string + str(self.sizey) + "\n"
+        string = string + str(len(self.stars)) + "\n"
+        for i in self.stars:
+            string = string + i.cache()
+        return string
+
+def rdf(f):
+    return input(file = f).replace("\n", "")
+
+def readMap(name):
+    Map = gameMap()
+    f = open(name)
+    
+    f.close()
