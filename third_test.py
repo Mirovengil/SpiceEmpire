0c71bb5bf504faa24c1cb0b7691557bf4113f73b
Шумилин Иван <mirovengil@gmail.com>
2021-05-29 09:23:26 +0300

    11/30 test.

--- third_test.py
+++ third_test.py
@@ -5,6 +5,7 @@
 import unittest
 import my_math
 from class_planet import Planet
+from class_ship import Ship
 
 class TestSaves (unittest.TestCase):
     '''
@@ -12,23 +13,21 @@ class TestSaves (unittest.TestCase):
     временным файлом) не происходит потери или искажения данных.
     '''
     def test_planet(self):
-        planet = Planet('Ioann', my_math.Coords(1, 2))
-        planet.limits = ('Средняя', 10)
-        planet.type = 'lave'
-        planet.image = 'image'
-        planet.master = Planet.NEITRAL
-        f = open('./log.txt', 'w')
+        planet = Planet.new_planet('test', my_math.Coords(1, 2))
+        f = open('log.txt', 'w')
         print(planet.cache(), file=f)
         f.close()
-        f = open('./log.txt', 'r')
+        f = open('log.txt', 'r')
         readed_planet = Planet.read_planet(f)
-        self.assertEqual(readed_planet.name, planet.name)
-        self.assertEqual(readed_planet.coordinates, planet.coordinates)
-        self.assertEqual(readed_planet.limits, planet.limits)
-        self.assertEqual(readed_planet.image, planet.image)
-        self.assertEqual(readed_planet.master, planet.master)
-        self.assertEqual(readed_planet.adi.will_be_occupied_by, planet.adi.will_be_occupied_by)
-        self.assertEqual(readed_planet.adi.will_be_occupied, planet.adi.will_be_occupied)
         f.close()
+        self.assertEqual(planet.limits, readed_planet.limits)
+        self.assertEqual(planet.name, readed_planet.name)
+        self.assertEqual(planet.coordinates, readed_planet.coordinates)
+        self.assertEqual(planet.type, readed_planet.type)
+        self.assertEqual(planet.image, readed_planet.image)
+        self.assertEqual(planet.master, readed_planet.master)
+        self.assertEqual(planet.description, readed_planet.description)
+        self.assertEqual(planet.adi.will_be_occupied, readed_planet.adi.will_be_occupied)
+        self.assertEqual(planet.adi.will_be_occupied_by, readed_planet.adi.will_be_occupied_by)
         
         
